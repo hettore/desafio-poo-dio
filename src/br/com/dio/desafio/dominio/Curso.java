f41fe3053c788bf7f428fa4b40ba114afef0c17f
Hettore Eduardo <duh.barretoo@gmail.com>
2022-11-23 13:57:38 -0300

    Criei a classe conteudo.

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,26 +1,17 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
+public class Curso extends Conteudo{
 
-	private String titulo;
-	private String descricao;
 	private int cargaHoraria;
+	
+	@Override
+	public double calcularXp() {
+		return XP_PADRAO * cargaHoraria;
+	}
 	
 	public Curso() {
 	}
 	
-	public String getTitulo() {
-		return titulo;
-	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
 	public int getCargaHoraria() {
 		return cargaHoraria;
 	}
@@ -30,8 +21,9 @@ public class Curso {
 	
 	@Override
 	public String toString() {
-		return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+		return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria + "]";
 	}
+
 	
 	
 }
